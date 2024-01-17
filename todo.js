669a8774ea75600693e2cac705c2ac6414733099
ChaMin <kusoft1997@gmail.com>
2024-01-17 22:53:54 +0900

    Feat]중요도 로직 구현

--- todo.js
+++ todo.js
@@ -1,23 +1,45 @@
 const toDoForm = document.querySelector(".toDoForm");
-const toDoInput = toDoForm.querySelector("input");
+const toDoBtn=document.querySelector(".toDoBtn");
+const toDoNum=document.querySelector(".toDoNum");
+const toDoNumInput = document.querySelector(".toDoImport")
+const toDoInput = document.querySelector(".toDoInput");
 const toDos = document.querySelector(".toDos");
 
-function paintToDo(toDo) {
+function paintToDo(toDo,num) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  span.id='todolist';
+  const span2 = document.createElement("span");
+  span2.id='import';
   span.innerHTML = toDo;
+  span2.innerHTML = num;
   li.appendChild(span);
+  li.appendChild(span2);
   toDos.appendChild(li);
 }
 
 function createToDo(event) {
   event.preventDefault();
   const toDo = toDoInput.value;
-  paintToDo(toDo);
+  const num=toDoNumInput.value;
+  if(num==""){
+    const def="1";
+    paintToDo(toDo,def);
+  }
+  else{
+    paintToDo(toDo,num);
+  }
+  console.log(toDo);
+  console.log(num);
+
   toDoInput.value = "";
+  toDoNumInput.value = "";
 }
 
 function init() {
   toDoForm.addEventListener("submit", createToDo);
+  toDoNum.addEventListener("submit",createToDo);
+  toDoBtn.addEventListener("click",createToDo);
+  
 }
 init();
